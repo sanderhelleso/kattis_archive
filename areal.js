b1a1137aed1a9ffee333ebdc9d7bcb95ce969ec6
Sander Hellesø <sanderhelleso@hotmail.com>
2019-04-23 21:09:04 -0700

    Create areal.js

---
+++ areal.js
@@ -0,0 +1,25 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {    
+    console.log(4 * Math.sqrt(parseInt(readLine())));
+}
