649b434121c512775615e4eba58c24919a7b0527
Sander Hellesø <sanderhelleso@hotmail.com>
2019-04-25 22:54:19 -0700

    Create kornislav.js

---
+++ kornislav.js
@@ -0,0 +1,27 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {        
+    
+    const edges = readLine().split(' ').map(Number).sort((a, b) => a - b);
+    console.log(edges[0] * edges[2]);
+}
