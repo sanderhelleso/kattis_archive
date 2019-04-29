883c9f9fb14442222aaa348b90d4ad2d39a3d8cf
Sander Hellesø <sanderhelleso@hotmail.com>
2019-04-28 20:28:32 -0700

    Create tetration.js

---
+++ tetration.js
@@ -0,0 +1,28 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    
+    const n = parseFloat(readLine());
+    console.log(Math.pow(n, 1 / n).toFixed(6))
+}
+
+
